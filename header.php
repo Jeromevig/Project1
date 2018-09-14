90016949c106087db5e260f2210449b3ce51348f
Marc-Antoine Castelain <ma.castelain@gmail.com>
2018-09-14 09:24:52 +0200

    Send pages.

---
+++ header.php
@@ -0,0 +1,25 @@
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
+  <a class="navbar-brand" href="#">Welcome to the Wild Code School of Lille  </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="collapsibleNavbar">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="/index.php">Presentation</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/students_page.php">Students</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/trainers_page.php">Trainers</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="euratech_page.php">EuraTechnologies</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="form_page.php">Contact</a>
+      </li>    
+    </ul>
+  </div>  
+</nav>
