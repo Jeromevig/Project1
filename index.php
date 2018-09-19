2a1bb9547a42025d8d92256bd64522a9b1fad2fb
Jeromevig <jer.vignerot@gmail.com>
2018-09-19 09:28:36 +0200

    Modification index.php.

--- index.php
+++ index.php
@@ -1,29 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <title>Project 1 - Group 1</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
-  <link rel="stylesheet" type="text/css" href="style.css">
-</head>
-<body>
-
-<?php include ("header.php") ?>
-
-<div class="container">
-  <div class="row">
-    <div class="col-sm-4">
-      <h2>About Me</h2>
-      <h5>Photo of me:</h5>
-      <div class="fakeimg">Fake Image</div>
-      <p>Some text about me in culpa qui officia deserunt mollit anim..</p>
-      <h3>Some Links</h3>
-      <p>Lorem ipsum dolor sit ame.</p>
-      <ul class="nav nav-pills flex-column">
+<head>ss="nav nav-pills flex-column">
         <li class="nav-item">
           <a class="nav-link active" href="#">Active</a>
         </li>
@@ -41,6 +18,29 @@
         </li>
       </ul>
       <hr class="d-sm-none">
+        <title>Project 1 - Group 1</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" type="text/css" href="style.css">
+        </head>
+        <body>
+
+        <?php include ("header.php") ?>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-4">
+                    <h2>About Me</h2>
+                    <h5>Photo of me:</h5>
+                    <div class="fakeimg">Fake Image</div>
+                    <p>Some text about me in culpa qui officia deserunt mollit anim..</p>
+                    <h3>Some Links</h3>
+                    <p>Lorem ipsum dolor sit ame.</p>
+                    <ul cla
     </div>
     <div class="col-sm-8">
       <h2>TITLE HEADING</h2>
