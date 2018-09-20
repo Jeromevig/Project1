068ea23895ebf4385a0dce0f3c699908e84e119f
Marc-Antoine Castelain <ma.castelain@gmail.com>
2018-09-21 00:20:53 +0200

    Send my work on header footer index students font and form.

--- students_page.php
+++ students_page.php
@@ -1,28 +1,113 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <!-- Required meta tags -->
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="/src/bootstrap-4.1.3/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="style.css">
-    <title>Project 1 - Group Noobs</title>
-</head>
-<body>
-    <?php include ("header.php") ?>
+<html lang="en" xmlns="http://www.w3.org/1999/html">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="author" content="Team Noobs - Group 1 - WCS">
+        <meta name="description" content="This is a humorous website that presents the atmosphere at Wild Code School">
+        <meta name="keywords" content="wild code school, lille, noobs">
+        <title>Project 1 - Group Noobs</title>
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="src/bootstrap-4.1.3/dist/css/bootstrap.min.css">
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <?php include ("header.php") ?>
 
-    <div><p>TON CODE ICI TONTON !</p></div>
+        <div class="container">
+            <div class="row">
+                <div class="col"
+                    <h1 style="text-align:center;font-weight: bold; font-size: 50px;">..::Observe the different profiles<br>of Wilders students during training::..</h1>
+                </div>
+            </div>
 
-    <?php include("footer.php") ?>
+            <div class="row">
+                <div class="col">
+                    <div class="card" style="width: 33rem;">
+                        <img class="card-img-top" src="public/rippcmonkey.gif" alt="Card image rip pc monkey">
+                        <div class="card-body">
+                            <p class="card-text">There are those who think that typing the same key on a keyboard for hours will turn their code into a gold mine ...</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card" style="width: 33rem;">
+                        <img class="card-img-top" src="public/frustrated.gif" alt="Card image rip pc monkey">
+                        <div class="card-body">
+                            <p class="card-text">There are those who creak psychologically under the weight of quests and projects.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <hr color="black">
+            <div class="row">
+                <div class="col">
+                    <div class="card" style="width: 33rem;">
+                        <img class="card-img-top" src="public/reading.gif" alt="Card image rip pc monkey">
+                        <div class="card-body">
+                            <p class="card-text">There are those who finish the exercises one week before the others and who can enjoy the small pleasures of life ...</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card" style="width: 33rem;">
+                        <img class="card-img-top" src="public/monkeycash.gif" alt="Card image rip pc monkey">
+                        <div class="card-body">
+                            <p class="card-text">There are those who protect their solutions as the apple of their eyes. We can believe that having a solution is equivalent to keeping a bundle of banknotes...</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <hr color="black">
+            <div class="row">
+                <div class="col">
+                    <div class="card" style="width: 33rem;">
+                        <img class="card-img-top" src="public/businessmonkeys.gif" alt="Card image rip pc monkey">
+                        <div class="card-body">
+                            <p class="card-text">There are those who understand that their working group is not meant to be a working group ...</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card" style="width: 33rem;">
+                        <img class="card-img-top" src="public/telmonkey.gif" alt="Card image rip pc monkey">
+                        <div class="card-body">
+                            <p class="card-text">There are those who prefer to discuss their love stories or their personal business rather than coding their project ... Is not it Samy ?</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <hr color="black">
+            <div class="row">
+                <div class="col">
+                    <div class="card" style="width: 33rem;">
+                        <img class="card-img-top" src="public/monkeymoney.gif" alt="Card image rip pc monkey">
+                        <div class="card-body">
+                            <p class="card-text">There are those who imagine that web development is above all a springboard to become a millionaire after these five months of training...</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card" style="width: 33rem;">
+                        <img class="card-img-top" src="public/slipgiphy.gif" alt="Card image rip pc monkey">
+                        <div class="card-body">
+                            <p class="card-text">But all Wilder have one thing in common. They share this passion for the code and the desire to get up every morning to make their contribution to the virtual building !</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <br>
 
-</body>
+        </div>
 
 
-<!-- Optional JavaScript -->
-<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-</body>
+        <?php include("footer.php") ?>
+
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    </body>
 </html>
