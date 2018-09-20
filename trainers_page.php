f242aab989d8d168779fa50f8864008361372187
antoinemta <antoine_mta@hotmail.fr>
2018-09-20 14:29:11 +0200

    I retired the absolute slash to absolute link in my page

--- trainers_page.php
+++ trainers_page.php
@@ -5,7 +5,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <!-- Bootstrap CSS -->
-        <link rel="stylesheet" href="/src/bootstrap-4.1.3/dist/css/bootstrap.min.css">
+        <link rel="stylesheet" href="src/bootstrap-4.1.3/dist/css/bootstrap.min.css">
         <link rel="stylesheet" href="style.css">
         <title>Project 1 - Group Noobs</title>
     </head>
