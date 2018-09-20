7ce1c8a421526bdc16caed6b9522e774e90a7444
Marc-Antoine Castelain <ma.castelain@gmail.com>
2018-09-20 13:25:38 +0200

    Clean code pages, header, footer and add pics.

--- index.php
+++ index.php
@@ -1,66 +1,137 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>ss="nav nav-pills flex-column">
-        <li class="nav-item">
-          <a class="nav-link active" href="#">Active</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Students</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Trainers</a>
-        </li>
-	<li class="nav-item">
-          <a class="nav-link" href="#">EuraTechnologies</a>
-        </li>
-	<li class="nav-item">
-          <a class="nav-link" href="#">Contact</a>
-        </li>
-      </ul>
-      <hr class="d-sm-none">
-        <title>Project 1 - Group 1</title>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
-        <link rel="stylesheet" type="text/css" href="style.css">
-        </head>
-        <body>
+<head>
+    <!-- Required meta tags -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="/src/bootstrap-4.1.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+    <title>Project 1 - Group Noobs</title>
+</head>
+<body>
+<?php include ("header.php") ?>
 
-        <?php include ("header.php") ?>
+    <section id="carousel">
+        <div class="container">
+            <div classe="row">
+                <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                    <ol class="carousel-indicators">
+                        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                    </ol>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img class="d-block w-100 h-100" style="border-radius: 5px;" src="/public/school1.jpg" alt="First slide">
+                            <div class="carousel-caption d-none d-md-block">
+                                <h5 style="color: deeppink; background-color: black; border-radius: 5px;">#Perfects-People</h5>
+                                <p style="color: deeppink; background-color: black; border-radius: 5px;">They are so cute, they are so resourceful...</p>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <img class="d-block w-100 h-100" style="border-radius: 5px;" src="/public/school2.jpg" alt="Second slide">
+                            <div class="carousel-caption d-none d-md-block">
+                                <h5 style="color: deeppink; background-color: black; border-radius: 5px;">#Need-An-Internship</h5>
+                                <p style="color: deeppink; background-color: black; border-radius: 5px;">They are full of love, they are at your disposal...</p>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <img class="d-block w-100 h-100" style="border-radius: 5px;" src="/public/school3.jpg" alt="Third slide">
+                            <div class="carousel-caption d-none d-md-block">
+                                <h5 style="color: deeppink; background-color: black; border-radius: 5px;">#To-Prove-Their-Value</h5>
+                                <p style="color: deeppink; background-color: black; border-radius: 5px;">Adopt your Wilder, hurry to contact one !</p>
+                            </div>
+                        </div>
+                    </div>
+                    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
+                </div>
+                <hr color="black">
+            </div>
+        </div>
+    </section>
 
+    <section id="presentation">
         <div class="container">
             <div class="row">
-                <div class="col-sm-4">
-                    <h2>About Me</h2>
-                    <h5>Photo of me:</h5>
-                    <div class="fakeimg">Fake Image</div>
-                    <p>Some text about me in culpa qui officia deserunt mollit anim..</p>
-                    <h3>Some Links</h3>
-                    <p>Lorem ipsum dolor sit ame.</p>
-                    <ul cla
-    </div>
+                <div class="col-md-6 background_case">
+                    <h2 style="padding-top: 15px; text-align:center;">Join the Wild lover community </h2>
+                    <p style="text-align: justify">
+                        Recruiting a developer is often a headache for businesses in an ever-changing digital marketplace.<br><br>
+                        Since then, the Wild Code School has moved into the market and offers intensive training as a web and/or mobile developer to provide professionals, in a minimum of time, THE Wilders.<br><br>
 
-    <div class="col-sm-8">
-      <h2>TITLE HEADING</h2>
-      <h5>Title description, Dec 7, 2017</h5>
-      <div class="fakeimg">Fake Image</div>
-      <p>Some text..</p>
-      <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
-      <br>
-      <h2>TITLE HEADING</h2>
-      <h5>Title description, Sep 2, 2017</h5>
-      <div class="fakeimg">Fake Image</div>
-      <p>Some text..</p>
-      <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
-    </div>
-  </div>
-</div>
+                        Each Wilder is a rare commodity, a kind of 21st century SUPERHERO.<br>
+                        He has powers that other human beings do not possess, including a SUPER-INTELLIGENCE, an extraordinary HUMANISM and a pronounced taste for CLEAN SOCKS.<br><br>
+
+                        In this regard, the students of the Wild Code School take off their shoes before entering the school.
+                        To take off one's shoes is to free oneself from the shackles of traditional education, which encloses, formats, and often prevents one from expressing oneself.
+                        When the feet are free, the mind is too !</p>
+
+                    <p style="text-align: justify">The Wild Code School of LIlle is currently thinking about changing the profile of its students. <br>
+                        More freedom ! Students in UNDERPANTS in order to code.<br><br>
+
+                    </p>
+                </div>
+                <div class="col-md-6">
+                    <img class="img-responsive w-100 h-100" src="/public/slippresentation.gif" alt="man_underpants">
+                </div>
+            </div>
+            <hr color="black">
+
+            <div class="row">
+                <div class="col-md">
+                    <img class="img-responsive w-100 h-100" src="/public/slippresentation.gif" alt="man_underpants">
+                </div>
+                <div class="col-md">
+                    <img class="img-responsive w-100 h-100" src="/public/slippresentation.gif" alt="man_underpants">
+                </div>
+                <div class="col-md">
+                    <img class="img-responsive w-100 h-100" src="/public/slippresentation.gif" alt="man_underpants">
+                </div>
+            </div>
+            <br>
+        </div>
+    </section>
+    <section id="illustration"
+        <div class="container">
+            <hr color="black">
+            <div class="row">
+                <p>Evolution d'un Wilder au cours de sa formation</p>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <img src="/public/rippcmonkey.gif" alt="monkey_tape_on_computer">
+                    <h3>Un noob en début de formation</h3>
+                </div>
+                <div class="col">
+                    <img src="/public/rippcmonkey.gif" alt="monkey_tape_on_computer">
+                    <h3>Un noob en début de formation</h3>
+                </div>
+                <div class="col">
+                    <img src="/public/rippcmonkey.gif" alt="monkey_tape_on_computer">
+                    <h3>Un noob en début de formation</h3>
+                </div>
+            </div>
+            <hr color="black">
+        </div>
+    </section>
 
 <?php include("footer.php") ?>
 
 </body>
-</html>
 
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</body>
+</html>
--- trainers_page.php
+++ trainers_page.php
@@ -1,67 +1,71 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <title>Bootstrap 4 Website Example</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
-  <link rel="stylesheet" type="text/css" href="style.css">
-</head>
-<body>
-<?php include ("header.php") ?>
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="/src/bootstrap-4.1.3/dist/css/bootstrap.min.css">
+        <link rel="stylesheet" href="style.css">
+        <title>Project 1 - Group Noobs</title>
+    </head>
+    <body>
+    <?php include ("header.php") ?>
 
-	<div class="row trainers">
-		<div class="col-12"><h1>The trainers !</h1></div>
-	</div>
+        <div class="row trainers">
+            <div class="col-12"><h1>The trainers !</h1></div>
+        </div>
 
-	<div class="row justify-content-center  heading">
-		<div class="col-4">
-			<center><div class="formatorA">
-				<img class="Formator1" src="Formator1.jpg"/>
-				</div></center>
-		</div>
-		<div class="col-4">
-			<center><div class="formatorB">
-				<img class="Formator2" src="Formator2.jpg"/>
-				</div></center>
-		</div>
-		<div class="col-4">
-			<center><div class="formatorC">
-				<img class="Formator3" src="Formator3.jpg"/>
-				</div></center>
-		</div>
-	</div>
-	
-	<div class="row justify-content-center heading">
-		<div class="col-10  div">
-			<div class="formatorBlock"></div>
-			<div class="formatorBlockA">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sodales pellentesque sapien vel sagittis. Suspendisse quis est velit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam erat volutpat. Etiam interdum porta neque sit amet sagittis. Morbi aliquet volutpat ultricies. Vivamus vitae ante dui. Curabitur nec hendrerit diam. <br>
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sodales pellentesque sapien vel sagittis. Suspendisse quis est velit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam erat volutpat. Etiam interdum porta neque sit amet sagittis. Morbi aliquet volutpat ultricies. Vivamus vitae ante dui. Curabitur nec hendrerit diam. 
-			</div>
-		</div>
-	</div>
+        <div class="row justify-content-center  heading">
+            <div class="col-4">
+                <center><div class="formatorA">
+                    <img class="Formator1" src="Formator1.jpg"/>
+                    </div></center>
+            </div>
+            <div class="col-4">
+                <center><div class="formatorB">
+                    <img class="Formator2" src="Formator2.jpg"/>
+                    </div></center>
+            </div>
+            <div class="col-4">
+                <center><div class="formatorC">
+                    <img class="Formator3" src="Formator3.jpg"/>
+                    </div></center>
+            </div>
+        </div>
 
-	<div class="row justify-content-center heading">
-		<div class="col-10  dev">
-			<div class="formatorBlockB">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sodales pellentesque sapien vel sagittis. Suspendisse quis est velit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam erat volutpat. Etiam interdum porta neque sit amet sagittis. Morbi aliquet volutpat ultricies. Vivamus vitae ante dui. Curabitur nec hendrerit diam. <br>
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sodales pellentesque sapien vel sagittis. Suspendisse quis est velit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam erat volutpat. Etiam interdum porta neque sit amet sagittis. Morbi aliquet volutpat ultricies. Vivamus vitae ante dui. Curabitur nec hendrerit diam.</div>
-			<div class="formatorBlock"></div>
-		</div>
-	</div>
-	
-	<div class="row justify-content-center headingg">
-		<div class="col-10  div">
-			<div class="formatorBlock"></div>
-			<div class="formatorBlockA">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sodales pellentesque sapien vel sagittis. Suspendisse quis est velit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam erat volutpat. Etiam interdum porta neque sit amet sagittis. Morbi aliquet volutpat ultricies. Vivamus vitae ante dui. Curabitur nec hendrerit diam.<br>
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sodales pellentesque sapien vel sagittis. Suspendisse quis est velit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam erat volutpat. Etiam interdum porta neque sit amet sagittis. Morbi aliquet volutpat ultricies. Vivamus vitae ante dui. Curabitur nec hendrerit diam. <br>
-		</div>
-		</div>		
-	</div>
+        <div class="row justify-content-center heading">
+            <div class="col-10  div">
+                <div class="formatorBlock"></div>
+                <div class="formatorBlockA">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sodales pellentesque sapien vel sagittis. Suspendisse quis est velit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam erat volutpat. Etiam interdum porta neque sit amet sagittis. Morbi aliquet volutpat ultricies. Vivamus vitae ante dui. Curabitur nec hendrerit diam. <br>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sodales pellentesque sapien vel sagittis. Suspendisse quis est velit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam erat volutpat. Etiam interdum porta neque sit amet sagittis. Morbi aliquet volutpat ultricies. Vivamus vitae ante dui. Curabitur nec hendrerit diam.
+                </div>
+            </div>
+        </div>
 
-<?php include("footer.php") ?>
+        <div class="row justify-content-center heading">
+            <div class="col-10  dev">
+                <div class="formatorBlockB">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sodales pellentesque sapien vel sagittis. Suspendisse quis est velit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam erat volutpat. Etiam interdum porta neque sit amet sagittis. Morbi aliquet volutpat ultricies. Vivamus vitae ante dui. Curabitur nec hendrerit diam. <br>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sodales pellentesque sapien vel sagittis. Suspendisse quis est velit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam erat volutpat. Etiam interdum porta neque sit amet sagittis. Morbi aliquet volutpat ultricies. Vivamus vitae ante dui. Curabitur nec hendrerit diam.</div>
+                <div class="formatorBlock"></div>
+            </div>
+        </div>
 
-</body>
+        <div class="row justify-content-center headingg">
+            <div class="col-10  div">
+                <div class="formatorBlock"></div>
+                <div class="formatorBlockA">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sodales pellentesque sapien vel sagittis. Suspendisse quis est velit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam erat volutpat. Etiam interdum porta neque sit amet sagittis. Morbi aliquet volutpat ultricies. Vivamus vitae ante dui. Curabitur nec hendrerit diam.<br>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sodales pellentesque sapien vel sagittis. Suspendisse quis est velit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam erat volutpat. Etiam interdum porta neque sit amet sagittis. Morbi aliquet volutpat ultricies. Vivamus vitae ante dui. Curabitur nec hendrerit diam. <br>
+            </div>
+            </div>
+        </div>
+
+    <?php include("footer.php") ?>
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    </body>
 </html>
